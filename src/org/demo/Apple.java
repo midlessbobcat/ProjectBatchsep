8925921fad4303449339028788ed56fe76227753
unknown <midlessbobcat>
2022-09-13 18:24:29 +0530

    add few lines of code

---
+++ src/org/demo/Apple.java
@@ -0,0 +1,23 @@
+package org.demo;
+
+public class Apple extends Demo{
+
+	public static void main(String[] args) {
+		
+			task1();
+			task2();
+		System.out.println(a);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/org/demo/Demo.java
@@ -0,0 +1,30 @@
+package org.demo;
+
+public  class Demo {
+
+	public static int a =10;
+	
+	
+	public static void task1() {
+		System.out.println("Task 1");
+
+	}
+	
+	public static void task2() {
+		System.out.println("task 2");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		task1();
+		task2();
+		System.out.println(a);
+		
+		
+	}
+	
+	
+	
+	
+}
