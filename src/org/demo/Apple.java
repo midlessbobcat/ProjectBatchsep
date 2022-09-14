f677da7251fd3e87224e195c13326cfd09c5df49
unknown <midlessbobcat>
2022-09-14 18:35:44 +0530

    Person1 completed

--- src/org/demo/Apple.java
+++ src/org/demo/Apple.java
@@ -2,7 +2,19 @@ package org.demo;
 
 public class Apple extends Demo{
 
+	private void person1() {
+		System.out.println("person 1");
+
+	}
+	
+	
 	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
 		
 			task1();
 			task2();
